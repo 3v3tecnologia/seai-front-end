a821379070c9be38859cf4cd1fb5f67e9c7a4041
Felipe Henrique <fh27.95@gmail.com>
2024-06-15 02:21:19 -0300

    fix: hidden reports, charts, weight & studies

--- src/router/routes/study.js
+++ src/router/routes/study.js
@@ -3,15 +3,15 @@ import BaseForm from "@/views/BaseForm.vue";
 import Studies from "@/views/Studies/Studies.vue";
 
 export default [
-  {
-    path: "/study",
-    name: "study",
-    meta: {
-      ShowNav: true,
-      title: "Estudos",
-      navBarTab: 10,
-    },
-    component: Studies,
-    props: studyUpdate,
-  },
+  // {
+  //   path: "/study",
+  //   name: "study",
+  //   meta: {
+  //     ShowNav: true,
+  //     title: "Estudos",
+  //     navBarTab: 10,
+  //   },
+  //   component: Studies,
+  //   props: studyUpdate,
+  // },
 ];
